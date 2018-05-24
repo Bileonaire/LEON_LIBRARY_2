88ff4c00cba5b7180e38a81f87aa88e48f7fd0b8
unknown <kiokoleon@gmail.com>
2018-05-24 09:46:48 +0300

    Fix Travis

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 """Contains various settings for each process of development
 """
-import os
+from os import getenv
 
 # basedir = os.path.abspath(os.path.dirname(__file__))
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'testing.db')
@@ -10,22 +10,23 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SECRET_KEY = "!@#_)&^%$$epic8^%%$#@#%^&*(&^&"
+    SECRET_KEY = getenv('SECRET_KEY')
 
 
 class TestingConfig(Config):
     """Contains additional config variables required during testing"""
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1Lomkones.@localhost/library_testing'
+    SQLALCHEMY_DATABASE_URI = getenv('TESTING_DATABASE_URI')
+    
 
 
 class DevelopmentConfig(Config):
     """Contains additional config variables required during development"""
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1Lomkones.@localhost/LEON-LIBRARYV2'
+    SQLALCHEMY_DATABASE_URI = getenv('DEVELOPMENT_DATABASE_URI')
 
 class ProductionConfig(Config):
     """Contains additional config variables required during production"""
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1Lomkones.@localhost/LEON-LIBRARYV2'    
+    SQLALCHEMY_DATABASE_URI = getenv('PRODUCTION_DATABASE_URI')   
